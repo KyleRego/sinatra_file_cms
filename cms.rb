e2f4280b65fd7c9537317766c446e02690d0b088
Kyle Rego <regoky@outlook.com>
2022-03-04 14:23:16 -0500

    Save work

--- cms.rb
+++ cms.rb
@@ -13,29 +13,51 @@ def render_markdown(text)
   markdown.render(text)
 end
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
 def load_file_content(path)
   content = File.read(path)
   case File.extname(path)
   when '.md'
     headers["Content-Type"] = "text/html"
-    render_markdown(content)
+    erb render_markdown(content)
   when '.txt'
     headers["Content-Type"] = "text/plain"
     content
   end
 end
 
-root = File.expand_path("..", __FILE__)
-
 get "/" do
-  data_path = root + "/data"
   @filenames = Dir.entries(data_path).select { |f| f != '.' && f != '..' }
   erb :index, layout: :layout
 end
 
+get "/new" do
+  erb :new, layout: :layout
+end
+
+post "/new" do
+  new_document_name = params[:new_document]
+  if new_document_name != ""
+    new_document = File.join(data_path, new_document_name)
+    File.open(new_document, 'w')
+    session[:success] = "#{new_document_name} was created."
+    redirect "/"
+  else
+    session[:error] = "A name is required."
+    redirect "/new"
+  end
+end
+
 get "/:filename" do
   filename = params[:filename]
-  file_path = root + "/data/#{filename}"
+  file_path = File.join(data_path, filename)
 
   if File.file?(file_path)
     load_file_content(file_path)
@@ -43,4 +65,30 @@ get "/:filename" do
     session[:error] = "#{filename} does not exist."
     redirect "/"
   end
+end
+
+post "/:filename/delete" do
+  filename = params[:filename]
+  file_path = File.join(data_path, filename)
+
+  if File.file?(file_path)
+    File.delete(file_path)
+    session[:success] = "#{filename} was deleted."
+  end
+  redirect "/"
+end
+
+get "/:filename/edit" do
+  @filename = params[:filename]
+  @contents = File.read(File.join(data_path, @filename))
+  erb :edit, layout: :layout
+end
+
+post "/:filename/edit" do
+  new_contents = params[:new_contents]
+  filename = params[:filename]
+  file_path = File.join(data_path, filename)
+  file = File.open(file_path, "w") { |f| f.write new_contents }
+  session[:success] = "#{filename} has been updated."
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -2,8 +2,15 @@ ENV["RACK_ENV"] = "test"
 
 require 'minitest/autorun'
 require 'rack/test'
+require 'fileutils'
 
 require_relative "../cms"
+
+def create_document(name, content = "")
+  File.open(File.join(data_path, name), 'w') do |file|
+    file.write content
+  end
+end
 
 class AppTest < MiniTest::Test
   include Rack::Test::Methods
@@ -12,16 +19,29 @@ class AppTest < MiniTest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
+
   end
 
   def test_history
+    create_document "history.txt", "the content of history.txt is this"
     get "/history.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
@@ -40,9 +60,66 @@ class AppTest < MiniTest::Test
   end
 
   def test_rendering_markdown_file
+    create_document "about.md", "# test markdown file"
     get "/about.md"
     assert_equal 200, last_response.status
     assert_equal "text/html", last_response["Content-Type"]
     assert_includes last_response.body, "<h1>test markdown file</h1>"
+  end
+
+  def test_editing_file
+    create_document "history.txt"
+
+    get "/history.txt/edit"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, "Save changes"
+
+    post "/history.txt/edit", new_contents: "history.txt: new contents"
+    assert_equal 302, last_response.status
+    
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "history.txt has been updated"
+
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new contents"
+  end
+
+  def test_adding_file
+    get "/new"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Add a new document:"
+
+    post "/new", new_document: "hello_world.txt"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "hello_world.txt"
+  end
+
+  def test_adding_file_with_no_name_error
+    post "/new", new_document: ""
+
+    get last_response["Location"]
+    assert_includes last_response.body, "A name is required."
+
+    get "/new"
+    refute_includes last_response.body, "A name is required."
+  end
+
+  def test_deleting_a_file
+    create_document "hello_world.txt"
+    get "/"
+    assert_includes last_response.body, "hello_world.txt"
+    post "/hello_world.txt/delete"
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_includes last_response.body, "hello_world.txt was deleted."
+    get "/"
+    refute_includes last_response.body, "hello_world.txt"
   end
 end
